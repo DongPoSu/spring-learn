a775ac8ab94037fcf7bdf445c9aa242c1533970f
苏政-java <851575021@qq.com>
2017-12-21 20:13:33 +0800

    initial module

---
+++ spring-learn-persistence-api/src/main/java/com/walkbear/spring/learn/convert/FullCutPromotionRequestConverter.java
@@ -0,0 +1,14 @@
+package com.walkbear.spring.learn.convert;
+
+/**
+ * @ClassName:com.sibu.shop.admin.convert
+ * @Author: wzl
+ * @Date: 2017/11/3
+ * @Description:
+ */
+public class FullCutPromotionRequestConverter {
+
+    public static void convertFrom(){
+
+    }
+}
---
+++ spring-learn-service/src/main/java/com/walkbear/spring/learn/service/TempService.java
@@ -0,0 +1,10 @@
+package com.walkbear.spring.learn.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qiujingwang
+ * Date: 2017-08-23
+ * Description:
+ */
+public interface TempService {
+}
